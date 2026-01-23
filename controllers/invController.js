31e096c398324a83017cdf28569fa02f07b38866
PelleyJ <pelleysam@gmail.com>
2026-01-24 01:36:17 +0900

    Assignment 3 completed

--- controllers/invController.js
+++ controllers/invController.js
@@ -4,18 +4,49 @@ const utilities = require("../utilities/")
 const invCont = {}
 
 /* ***************************
- *  Build inventory by classification view
+ * Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId
   const data = await invModel.getInventoryByClassificationId(classification_id)
+
+  if (!data || data.length === 0) {
+    const err = new Error("No vehicles found for that classification.")
+    err.status = 404
+    throw err
+  }
+
   const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
   const className = data[0].classification_name
+
   res.render("./inventory/classification", {
     title: className + " vehicles",
     nav,
     grid,
+  })
+}
+
+/* ***************************
+ * Build inventory detail view by inventory id
+ * ************************** */
+invCont.buildByInventoryId = async function (req, res, next) {
+  const invId = req.params.invId
+  const data = await invModel.getInventoryById(invId)
+
+  if (!data) {
+    const err = new Error("Vehicle not found.")
+    err.status = 404
+    throw err
+  }
+
+  const detail = await utilities.buildVehicleDetail(data)
+  let nav = await utilities.getNav()
+
+  res.render("./inventory/detail", {
+    title: `${data.inv_make} ${data.inv_model}`,
+    nav,
+    detail,
   })
 }
 
--- utilities/index.js
+++ utilities/index.js
@@ -2,6 +2,16 @@ const invModel = require("../models/inventory-model")
 const Util = {}
 
 /* ************************
+ * Middleware For Handling Errors
+ * Wraps async route/controller functions so errors go to Express error handler
+ ************************** */
+Util.handleErrors = function (fn) {
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next)
+  }
+}
+
+/* ************************
  * Constructs the nav HTML unordered list
  ************************** */
 Util.getNav = async function (req, res, next) {
@@ -75,6 +85,37 @@ Util.buildClassificationGrid = async function (data) {
     grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>'
   }
   return grid
+}
+
+/* **************************************
+ * Build the vehicle detail view HTML
+ * ************************************ */
+Util.buildVehicleDetail = async function (data) {
+  const price = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(data.inv_price)
+
+  const miles = new Intl.NumberFormat("en-US").format(data.inv_miles)
+
+  return `
+    <section class="vehicle-detail">
+      <div class="vehicle-detail__image">
+        <img src="${data.inv_image}" alt="${data.inv_year} ${data.inv_make} ${data.inv_model}">
+      </div>
+
+      <div class="vehicle-detail__info">
+        <h2>${data.inv_year} ${data.inv_make} ${data.inv_model}</h2>
+
+        <p class="vehicle-detail__price"><strong>Price:</strong> ${price}</p>
+        <p><strong>Mileage:</strong> ${miles} miles</p>
+        <p><strong>Color:</strong> ${data.inv_color}</p>
+
+        <h3>Description</h3>
+        <p>${data.inv_description}</p>
+      </div>
+    </section>
+  `
 }
 
 module.exports = Util
